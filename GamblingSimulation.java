556698760e945aa6b37e305d924b1e324a6175df
aamir7shahab <aamir.shahab007@gmail.com>
2020-08-19 20:54:07 +0530

    First commit and Initialised constant variables.

---
+++ GamblingSimulation.java
@@ -0,0 +1,11 @@
+  
+class GamblingSimulation {
+	//Constant
+	public static final int STARTING_DAILY_STAKE = 100;
+	public static final int BET = 1;
+
+	public static void main(String[] args) {
+		System.out.println("Starting Daily Stake : "+ STARTING_DAILY_STAKE);
+		System.out.println("Starting Daily Stake : "+ BET);
+	}
+}
